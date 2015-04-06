f9cf390e88c0890659aa4caf3362342772b13bec
Ovidiu Cherecheș <skidding@gmail.com>
2015-04-06 18:01:21 +0300

    Reuse tests between entry points

--- tests/router.js
+++ tests/router.js
@@ -12,33 +12,13 @@ describe('Router class', function() {
   var componentInstance = {},
       onRenderSpy = sinon.spy(function() {
         return componentInstance;
-      });
-
-  var routerOptions,
+      }),
+      routerOptions,
       routerInstance,
       location,
       uriParams;
 
-  function createRouter(extraOptions) {
-    routerInstance = new Router(_.merge(routerOptions, extraOptions));
-  };
-
-  beforeEach(function() {
-    // Allow query string to be overridden before tests
-    location = 'mypage.com?component=List&dataUrl=users.json';
-
-    uriParams = {
-      component: 'List',
-      props: {
-        dataUrl: 'users.json'
-      }
-    };
-
-    sinon.stub(uri, 'parseLocation', function() {
-      return uriParams;
-    });
-
-    // Ignore window API
+  var stubWindowApi = function() {
     sinon.stub(Router.prototype, '_getCurrentLocation', function() {
       return location;
     });
@@ -47,86 +27,95 @@ describe('Router class', function() {
     sinon.stub(Router.prototype, '_unbindPopStateEvent');
     sinon.stub(Router.prototype, '_replaceHistoryState');
     sinon.stub(Router.prototype, '_pushHistoryState');
+  };
 
-    routerOptions = {onRender: onRenderSpy};
-  });
-
-  afterEach(function() {
-    uri.parseLocation.restore();
-
+  var restoreWindowApi = function() {
     Router.prototype._getCurrentLocation.restore();
     Router.prototype._isPushStateSupported.restore();
     Router.prototype._bindPopStateEvent.restore();
     Router.prototype._unbindPopStateEvent.restore();
     Router.prototype._replaceHistoryState.restore();
     Router.prototype._pushHistoryState.restore();
+  };
+
+  var genericTests = function() {
+    it('should unserialize location', function() {
+      expect(uri.parseLocation).to.have.been.calledWith(location);
+    });
+
+    it('should attach router reference to props', function() {
+      expect(onRenderSpy.lastCall.args[0].router).to.equal(routerInstance);
+    });
+
+    it('should use container node received in options', function() {
+      expect(onRenderSpy.lastCall.args[1]).to.equal('<fake DOM element>');
+    });
+
+    it('should render using URL params as props', function() {
+      var propsSent = onRenderSpy.lastCall.args[0];
+      expect(propsSent.dataUrl).to.equal(uriParams.props.dataUrl);
+    });
+
+    it('should extend default props', function() {
+      var props = onRenderSpy.lastCall.args[0];
+      expect(props.dataUrl).to.equal(uriParams.props.dataUrl);
+      expect(props.defaultProp).to.equal(true);
+    });
+
+    it('should expose reference to root component', function() {
+      expect(routerInstance.rootComponent).to.equal(componentInstance);
+    });
+
+    it('should call onChange callback with params', function() {
+      var params = routerOptions.onChange.lastCall.args[0];
+      expect(params.component).to.equal(uriParams.component);
+      expect(params.props.dataUrl).to.equal(uriParams.props.dataUrl);
+    });
+  };
+
+  beforeEach(function() {
+    stubWindowApi();
+
+    sinon.stub(uri, 'parseLocation', function() {
+      return uriParams;
+    });
+
+    routerOptions = {
+      defaultProps: {
+        defaultProp: true
+      },
+      container: '<fake DOM element>',
+      onRender: onRenderSpy,
+      onChange: sinon.spy()
+    };
+  });
+
+  afterEach(function() {
+    restoreWindowApi();
+
+    uri.parseLocation.restore();
 
     onRenderSpy.reset();
   });
 
   describe('new instance', function() {
-    it('should get current location', function() {
-      createRouter();
+    beforeEach(function() {
+      location = 'mypage.com?component=List&dataUrl=users.json';
 
-      expect(routerInstance._getCurrentLocation).to.have.been.called;
-    });
-
-    it('should unserialize current URL', function() {
-      createRouter();
-
-      expect(uri.parseLocation).to.have.been.calledWith(location);
-    });
-
-    it('should render using URL params as props', function() {
-      createRouter();
-
-      var propsSent = onRenderSpy.lastCall.args[0];
-      expect(propsSent.dataUrl).to.equal('users.json');
-    });
-
-    it('should extend default props', function() {
-      createRouter({
-        defaultProps: {
-          defaultProp: true
+      uriParams = {
+        component: 'List',
+        props: {
+          dataUrl: 'users.json'
         }
-      });
+      };
 
-      var propsSent = onRenderSpy.lastCall.args[0];
-      expect(propsSent.dataUrl).to.equal('users.json');
-      expect(propsSent.defaultProp).to.equal(true);
+      routerInstance = new Router(routerOptions);
     });
 
-    it('should attach router reference to props', function() {
-      createRouter();
+    genericTests();
 
-      var propsSent = onRenderSpy.lastCall.args[0];
-      expect(propsSent.router).to.equal(routerInstance);
-    });
-
-    it('should use container node received in options', function() {
-      var container = '<fake DOM element>';
-      createRouter({
-        container: container
-      });
-
-      expect(onRenderSpy.lastCall.args[1]).to.equal(container);
-    });
-
-    it('should expose reference to root component', function() {
-      createRouter();
-
-      expect(routerInstance.rootComponent).to.equal(componentInstance);
-    });
-
-    it('should call onChange callback with params', function() {
-      var onChangeSpy = sinon.spy();
-      createRouter({
-        onChange: onChangeSpy
-      });
-
-      var propsSent = onChangeSpy.lastCall.args[0];
-      expect(propsSent.component).to.equal('List');
-      expect(propsSent.props.dataUrl).to.equal('users.json');
+    it('should get current location', function() {
+      expect(routerInstance._getCurrentLocation).to.have.been.called;
     });
   });
 
@@ -140,79 +129,23 @@ describe('Router class', function() {
           dataUrl: 'user.json'
         }
       };
+
+      routerInstance = new Router(routerOptions);
+      routerInstance.goTo(location);
     });
 
+    genericTests();
+
     it('should check if pushState is supported', function() {
-      createRouter();
-
-      routerInstance.goTo(location);
-
       expect(routerInstance._isPushStateSupported).to.have.been.called;
     });
 
-    it('should unserialize new URL query string', function() {
-      createRouter();
-
-      routerInstance.goTo(location);
-
-      expect(uri.parseLocation).to.have.been.calledWith(location);
-    });
-
-    it('should render using new URL params as props', function() {
-      createRouter();
-
-      routerInstance.goTo(location);
-
-      var propsSent = onRenderSpy.lastCall.args[0];
-      expect(propsSent.dataUrl).to.equal('user.json');
-    });
-
-    it('should extend default props', function() {
-      createRouter({
-        defaultProps: {
-          defaultProp: true
-        }
-      });
-
-      routerInstance.goTo(location);
-
-      var propsSent = onRenderSpy.lastCall.args[0];
-      expect(propsSent.dataUrl).to.equal('user.json');
-      expect(propsSent.defaultProp).to.equal(true);
-    });
-
-    it('should attach router reference to props', function() {
-      createRouter();
-
-      routerInstance.goTo(location);
-
-      var propsSent = onRenderSpy.lastCall.args[0];
-      expect(propsSent.router).to.equal(routerInstance);
-    });
-
     it('should push new entry to browser history', function() {
-      createRouter();
-
-      routerInstance.goTo(location);
-
       // It's a bit difficult to mock the native functions so we mocked the
       // private methods that wrap those calls
       //expect(routerInstance._pushHistoryState).to.have.been.called;
       expect(routerInstance._pushHistoryState.lastCall.args[1])
           .to.equal(location);
-    });
-
-    it('should call onChange callback', function() {
-      var onChangeSpy = sinon.spy();
-      createRouter({
-        onChange: onChangeSpy
-      });
-
-      routerInstance.goTo(location);
-
-      var propsSent = onChangeSpy.lastCall.args[0];
-      expect(propsSent.component).to.equal('User');
-      expect(propsSent.props.dataUrl).to.equal('user.json');
     });
   });
 
@@ -226,60 +159,17 @@ describe('Router class', function() {
           dataUrl: 'user.json'
         }
       };
+
+      routerInstance = new Router(routerOptions);
+      routerInstance.onPopState({
+        state: {}
+      });
     });
+
+    genericTests();
 
     it('should get current location', function() {
-      createRouter();
-
       expect(routerInstance._getCurrentLocation).to.have.been.called;
-    });
-
-    it('should unserialize current URL', function() {
-      createRouter();
-
-      expect(uri.parseLocation).to.have.been.calledWith(location);
-    });
-
-    it('should extend default props', function() {
-      createRouter({
-        defaultProps: {
-          defaultProp: true
-        }
-      });
-
-      routerInstance.onPopState({
-        state: {}
-      });
-
-      var propsSent = onRenderSpy.lastCall.args[0];
-      expect(propsSent.dataUrl).to.equal('user.json');
-      expect(propsSent.defaultProp).to.equal(true);
-    });
-
-    it('should attach router reference to props', function() {
-      createRouter();
-
-      routerInstance.onPopState({
-        state: {}
-      });
-
-      var propsSent = onRenderSpy.lastCall.args[0];
-      expect(propsSent.router).to.equal(routerInstance);
-    });
-
-    it('should call onChange callback', function() {
-      var onChangeSpy = sinon.spy();
-      createRouter({
-        onChange: onChangeSpy
-      });
-
-      routerInstance.onPopState({
-        state: {}
-      });
-
-      var propsSent = onChangeSpy.lastCall.args[0];
-      expect(propsSent.component).to.equal('User');
-      expect(propsSent.props.dataUrl).to.equal('user.json');
     });
   });
 });
